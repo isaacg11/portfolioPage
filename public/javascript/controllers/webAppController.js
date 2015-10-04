ac893e31267cc5ae98788ce99d362914e65f1d05
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-03 20:59:20 -0700

    added onto the web app submission form

--- public/javascript/controllers/webAppController.js
+++ public/javascript/controllers/webAppController.js
@@ -21,5 +21,15 @@
 	});
 };
 	//-------------------------------------------------------------------------//
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
+
+
+
+
+	//-------------------------------------------------------------------------//
 	}
 })();
